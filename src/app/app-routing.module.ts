b462d647cf9318cd4dcb4b84c6ceb5a43ab6d871
me <techwithulises@gmail.com>
2023-08-04 11:45:44 -0600

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ApiOrganizationsComponent } from './components/api-organizations/api-organizations.component';
+import { ApiNetworksComponent } from './components/api-networks/api-networks.component';
+import { ApiDevicesComponent } from './components/api-devices/api-devices.component';
+import { ApiClientsSearchMacComponent } from './components/api-clients-search-mac/api-clients-search-mac.component';
+import { DbOrganizationsComponent } from './components/db-organizations/db-organizations.component';
+import { DbClientsSearchMacComponent } from './components/db-clients-search-mac/db-clients-search-mac.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'api/organizations', component: ApiOrganizationsComponent },
+  { path: 'api/networks', component: ApiNetworksComponent },
+  { path: 'api/devices', component: ApiDevicesComponent },
+  { path: 'api/clients/search/mac', component: ApiClientsSearchMacComponent },
+  { path: 'db/organizations', component: DbOrganizationsComponent },
+  { path: 'db/clients/search/mac', component: DbClientsSearchMacComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
