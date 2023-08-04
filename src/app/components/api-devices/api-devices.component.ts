8e639e29c98a9536ca83378540a195874974a80d
me <techwithulises@gmail.com>
2023-08-04 12:04:33 -0600

    devices

--- src/app/components/api-devices/api-devices.component.ts
+++ src/app/components/api-devices/api-devices.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Device {
+  name: string;
+  serial: string;
+  mac: string;
+  networkId: string;
+  productType: string;
+  model: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-api-devices',
   templateUrl: './api-devices.component.html',
   styleUrls: ['./api-devices.component.css']
 })
-export class ApiDevicesComponent {
+export class ApiDevicesComponent implements OnInit {
+  dispositivos: Device[] = [];
+  error: string | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const orgId = '1002375';
+    this.getDispositivos(orgId);
+  }
+
+  getDispositivos(orgId: string) {
+    this.http.get<Device[]>(`http://localhost:3000/api/organizations/${orgId}/allDevices`)
+      .subscribe(
+        (data: Device[]) => {
+          this.dispositivos = data;
+        },
+        (error) => {
+          this.error = 'Error al obtener los dispositivos: ' + error.message;
+        }
+      );
+  }
 }
