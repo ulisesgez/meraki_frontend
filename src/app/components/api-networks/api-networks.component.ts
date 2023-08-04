dd9f38b47f7d5e447843b4ab1b686aa6e9436bca
me <techwithulises@gmail.com>
2023-08-04 12:01:42 -0600

    networks

--- src/app/components/api-networks/api-networks.component.ts
+++ src/app/components/api-networks/api-networks.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Network {
+  id: number;
+  organizationId: number;
+}
 
 @Component({
   selector: 'app-api-networks',
   templateUrl: './api-networks.component.html',
   styleUrls: ['./api-networks.component.css']
 })
-export class ApiNetworksComponent {
+export class ApiNetworksComponent implements OnInit {
+  redes: Network[] = [];
+  error: string | null = null;
 
+  constructor(private http: HttpClient) {} // Inyectar HttpClient en el constructor
+
+  ngOnInit() {
+    const orgId = '1002375'; // Reemplaza esto con el ID de la organización deseada
+    this.getRedes(orgId);
+  }
+
+  getRedes(orgId: string) {
+    this.http.get<Network[]>(`http://localhost:3000/api/organizations/${orgId}/networks`)
+      .subscribe(
+        (data: Network[]) => {
+          this.redes = data;
+        },
+        (error) => {
+          this.error = 'Error al obtener las redes: ' + error.message;
+        }
+      );
+  }
 }
