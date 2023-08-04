486a6c271678d161e9c648f934d8423fe6a72ea1
me <techwithulises@gmail.com>
2023-08-04 11:58:13 -0600

    organizations

--- src/app/components/api-organizations/api-organizations.component.ts
+++ src/app/components/api-organizations/api-organizations.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Organization {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-api-organizations',
   templateUrl: './api-organizations.component.html',
   styleUrls: ['./api-organizations.component.css']
 })
-export class ApiOrganizationsComponent {
+export class ApiOrganizationsComponent implements OnInit {
+  organizaciones: Organization[] = [];
+  error: string | null = null;
 
+  constructor(private http: HttpClient) {} // Inyectar HttpClient en el constructor
+
+  ngOnInit() {
+    this.getOrganizaciones();
+  }
+
+  getOrganizaciones() {
+    this.http.get<Organization[]>('http://localhost:3000/api/organizations')
+      .subscribe(
+        data => {
+          this.organizaciones = data;
+        },
+        error => {
+          this.error = 'Error al obtener las organizaciones: ' + error.message;
+        }
+      );
+  }
 }
