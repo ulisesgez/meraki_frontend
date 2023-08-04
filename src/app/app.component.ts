fb6d6a2a330523cab5a283ca3878408b3b399348
me <techwithulises@gmail.com>
2023-08-04 11:34:54 -0600

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'meraki_frontend';
+
+  showMenu: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
